4b6abd0c7e54f1da73cdfb71ee592cc7f16ff8fe
Ivan <ivkozlov@htsts.ru>
2020-07-13 14:42:28 +0300

    step 4

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 var readlineSync = require('readline-sync');
 
-function userName() {
+export function userName() {
     const userName = readlineSync.question('May I have your name? ');
     console.log('Hello ' + userName + '!');
 }
 
-export default userName;
+const randomNumber = function (min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min); 
+};
+  
+export function gameone () {
+      var attempts;
+      for (attempts = 0; attempts < 3; attempts++){
+      let question = randomNumber(1,100);
+      console.log(question);
+      let num = readlineSync.question ( 'What is your answer? (y/n)');
+      if (question % 2 == 0 && num == 'y'){
+          console.log ('Correct!');
+      } 
+       else if (question % 2 == 0 && num == 'n'){
+          console.log ('Correct!');
+       }
+       else if (( question % 2 != 0 && num === 'y ') || ( question % 2 == 0 && num == 'n' )){
+            console.log ('Wrong answer!');
+        }
+        else {
+            console.log ('It`s wrong input');
+        }
+    }
+}
--- src/brain-games.js
+++ src/brain-games.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
-import userName from '../index'
+import {userName,gameone} from ".."
 
 let Welcome = () => console.log("Welcome to the Brain Games!");
 Welcome();
 userName();
-
+const parCheck = () => console.log('Answer "yes" if number even otherwise answer "no"');
+parCheck();
+gameone();
